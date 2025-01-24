988019bbd8cd25e6cae17f7d6a7c8639ec0e38c7
blacerdc <dionnecatherine.blacer.cics@ust.edu.ph>
2025-01-24 17:11:42 +0800

    sign up page .html -> .php

--- User_SignupPage.php
+++ User_SignupPage.php
@@ -83,9 +83,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <!-- Required -->
 
   <div class="navbar">
-    <a href="User_Homepage.html">About</a>
-    <a href="User_Contactpage.html">Contact</a>
-    <a href="User_Showcase.html">Showcase</a>
+    <a href="User_Homepage.php">About</a>
+    <a href="User_Contactpage.php">Contact</a>
+    <a href="User_Showcase.php">Showcase</a>
   </div>
 
   <div class="logo">
@@ -100,15 +100,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <br />
     <ul id="dropdown1" class="dropdown-content" style="transition: 0.3s; display: none; position: absolute; top: 60px; right: 0; background-color: white; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15); width: 200px; padding: 0; margin: 0;">
         <li style="list-style: none; margin: 0; padding: 10px; transition: 0.3s;">
-            <a href="User_LoginPage.html" style="color: black; text-decoration: none; display: block; padding: 5px 10px;">Login</a>
+            <a href="User_LoginPage.php" style="color: black; text-decoration: none; display: block; padding: 5px 10px;">Login</a>
         </li>
         <li style="list-style: none; margin: 0; padding: 10px; transition: 0.3s;">
-            <a href="User_SignupPage.html" style="color: black; text-decoration: none; display: block; padding: 5px 10px;">Sign Up</a>
+            <a href="User_SignupPage.php" style="color: black; text-decoration: none; display: block; padding: 5px 10px;">Sign Up</a>
         </li>
     </ul>
   </div>
 
-  <a href="User_InquiryPage.html">
+  <a href="User_InquiryPage.php">
       <img src="Assets/icon_FAQ.png" alt="Inquiry Icon" class="inquiry-icon" />
   </a>
 
@@ -149,7 +149,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 </div>
 
                 <button class="btn_login" type="submit" name="action" id="signup"> Sign Up </button>
-                <p class="existingacc"> Already have an account? <a href="User_LoginPage.html" class="a1"> Login. </a> </p>
+                <p class="existingacc"> Already have an account? <a href="User_LoginPage.php" class="a1"> Login. </a> </p>
             </form>
 
             <!-- Display error or success messages -->
